66d047cbc26024aa6a242db8e1990ef1e6da2eec
Auvik <auvik@mousepotato.co>
2025-07-11 12:48:19 -0400

    updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,9 +8,10 @@ import TaskSection from "./TaskSection";
 
 const Dashboard = () => {
 	return (
-		<div className="max-w-[120rem] h-screen w-full mx-auto">
-			<NavBar />
-			{/* <div className="p-4 max-w-[90rem] flex flex-col mx-auto size-full">
+		<div className="max-w-[120rem] h-screen w-full mx-auto flex flex-col justify-between">
+			<div>
+				<NavBar />
+				{/* <div className="p-4 max-w-[90rem] flex flex-col mx-auto size-full">
 				<div className="flex gap-4 flex-wrap sm:flex-nowrap">
 					<TaskSection title="To Do" />
 
@@ -19,13 +20,16 @@ const Dashboard = () => {
 				<ProgressSection />
 			</div> */}
 
-			<div className="flex gap-2 px-2 flex-wrap sm:flex-nowrap">
-				<TaskSection title="To Do" />
-				<RememberSection title="To Remember" />
-				<ProgressSection />
+				<div className="flex gap-2 px-2 flex-wrap sm:flex-nowrap">
+					<TaskSection title="To Do" />
+					<RememberSection title="To Remember" />
+					<ProgressSection />
+				</div>
 			</div>
 
-			<Footer />
+			<div>
+				<Footer />
+			</div>
 		</div>
 	);
 };
