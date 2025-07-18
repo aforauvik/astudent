a3cd0c86837dab04d58e99ba51ce5d4589af40c5
Auvik <auvik@mousepotato.co>
2025-07-17 23:14:48 -0400

    pwa update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-import withPWA from "next-pwa";
+import createPWA from "next-pwa";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default withPWA({
-	...nextConfig,
-	pwa: {
-		dest: "public",
-		register: true,
-		skipWaiting: true,
-		disable: process.env.NODE_ENV === "development",
-	},
+const withPWA = createPWA({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
 });
+
+const nextConfig = {
+	// Add any other Next.js config options here
+};
+
+export default withPWA(nextConfig);
